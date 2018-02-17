4661af5fb077191f01eb9490d337c2eaa587f4b7
Rebecca Burton <rebeccabexb@gmail.com>
2018-02-17 19:23:56 +0000

    Added page to set cricket teams

--- app/controllers/Cricket.scala
+++ app/controllers/Cricket.scala
@@ -5,7 +5,9 @@ import play.api.mvc._
 class Cricket extends Controller {
 
   def start = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.blankPage("Planned technical updates. Please come back later."))
+    Ok(views.html.cricketMain("Setup page"))
   }
+
+
 
 }
