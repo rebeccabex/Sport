855457c41e5f702113c6a76f99de6f19cfc82901
Rebecca Burton <rebeccabexb@gmail.com>
2018-02-18 16:15:51 +0000

    Created basic implementation of cricketScoreboard

--- app/controllers/Cricket.scala
+++ app/controllers/Cricket.scala
@@ -1,13 +1,36 @@
 package controllers
 
 import play.api.mvc._
+import models.cricket.CricketMatch.currentMatch
+import models.cricket.CricketPlayer
+import models.cricket.CricketTeam
+import models.cricket.Innings.blankInnings
+import models.cricket.BatStats.blankBatStats
+import models.cricket.BowlStats.blankBowlStats
+import models.cricket.FieldStats.blankFieldStats
 
 class Cricket extends Controller {
 
-  def start = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.cricketMain("Setup page"))
+  def setup = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.cricketMain(currentMatch))
   }
 
+  def startMatch = Action { implicit request: Request[AnyContent] =>
+    val matchFormData = request.body.asFormUrlEncoded
 
+    for (i <- 1 to 2) {
+      val players = new Array[CricketPlayer](11)
+      for (j <- 1 to 11) {
+        val playerName = matchFormData.get(s"team${i}Player${j}").head
+        val player = CricketPlayer(j, 12-j, false, playerName, Array(blankBatStats), Array(blankBowlStats), Array(blankFieldStats))
+        players(j-1) = player
+      }
+      val teamName = matchFormData.get(s"team${i}").head
+      val teamRating = matchFormData.get(s"team${i}Rating").head.toInt
+      val team = CricketTeam(teamName, teamRating, players, Array(blankInnings))
+      currentMatch.teams(i-1) = team
+    }
+    Ok(views.html.cricketScoreboard(currentMatch))
+  }
 
 }
--- app/models/cricket/CricketMatch.scala
+++ app/models/cricket/CricketMatch.scala
@@ -3,6 +3,8 @@ package models.cricket
 case class CricketMatch(teams: Array[CricketTeam], noOfInnings: Int, oversPerInnings: Int, noOfDays: Int, oversPerDay: Int) {}
 
 object CricketMatch {
-  val blankMatch = CricketMatch(Array(), 0, 0, 0, 0)
+  val currentMatch = CricketMatch(Array(CricketTeam.blankTeam, CricketTeam.blankTeam), 0, 0, 0, 0)
+
+
 }
 
